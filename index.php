08d538291b8802f8f48120784b56f51609139b2a
CharaVs <kristersvolkovs11@gmail.com>
2025-02-14 09:47:40 +0000

    Test

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Project</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="divBox">
+        <div class="leftBox">
+            <div class="boxContainer">
+                <h1>Status</h1>
+            </div>
+            <hr></hr>
+            <div class="boxContainer">
+                <h1>Thing</h1>
+            </div>
+        </div>
+        <div class="rightBox">
+        <div class="boxContainer" style="width: 74vw;">
+                <h1>Box1</h1>
+                <h1>Box2</h1>
+                <h1>Box1</h1>
+                <h1>Box2</h1>
+                <h1>Box1</h1>
+                <h1>Box2</h1>
+                <h1>Box1</h1>
+                <h1>Box2</h1>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
